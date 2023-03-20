84f01f0ce6b5d6995538caeb9b3848bb0b907351
秋 <1434707902@qq.com>
2023-03-20 23:11:39 +0800

    3.20

---
+++ command.py
@@ -0,0 +1,26 @@
+import qq
+from qq.ext import commands
+from config import appid, token
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+client = qq.Client()
+
+intent = qq.Intents.default()
+intent.guild_messages = True
+intent.at_guild_messages = False
+bot = commands.Bot(command_prefix="/", owner_id=114514, intents=intent)
+
+
+@bot.command(name="组合")
+async def test(ctx: commands.Context, one: str, two: str):
+    await ctx.reply(one + two)
+
+
+@bot.command(name="重复")
+async def test(ctx: commands.Context, one: str):
+    await ctx.reply(one)
+
+
+if __name__ == '__main__':
+    bot.run(token=f"{appid}.{token}")
